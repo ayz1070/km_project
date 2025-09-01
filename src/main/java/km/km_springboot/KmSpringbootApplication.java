1aed0a9e5bddd04835c604b70564866217a7ecee
ayz1070 <ayz1070@gmail.com>
2025-09-01 19:59:48 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/km/km_springboot/KmSpringbootApplication.java
@@ -0,0 +1,13 @@
+package km.km_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/km/km_springboot/KmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package km.km_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
